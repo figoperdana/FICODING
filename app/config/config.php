939470ee12d8de6a0dd9faed23f7ba07a9e48ed9
Figo Perdana Putra <57206216+figoperdana@users.noreply.github.com>
2022-08-05 12:19:07 +0700

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -5,7 +5,7 @@ $root .= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAM
 define('BASEURL', $root);
 
 // DB
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'ficoding');
+define('DB_HOST', 'us-cdbr-east-06.cleardb.net');
+define('DB_USER', 'bb56e5c72c1b8a');
+define('DB_PASS', 'e6afea9f');
+define('DB_NAME', 'heroku_38bfe8e5b9a810e');
